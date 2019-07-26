a038f4665b1efa63cfca6826ab58abb558d930ca
Christina Park <christinapark0117@gmail.com>
2019-07-26 19:49:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  a+b;
+  return a+b; 
+}
+
+function subtract(a, b) {
+  a-b;
+  return a-b;
+}
+
+function multiply(a, b) {
+  a*b;
+  return a*b;
+}
+
+function divide(a, b) {
+  a/b;
+  return a/b;
+}
+
+function inc(n) {
+  n++;
+  return n++;
+}
+
+function dec(n) {
+  n--;
+  return n--;
+}
+
+function makeInt(n) {
+  parseInt('2', 10)
+  return 2
+}
